ca5f9589326a71134d129569322c18e59f767609
Christian Hapgood <christian.hapgood@gmail.com>
2021-11-15 01:01:54 +0000

    Add matrix, uniforms

--- Tutorial02/app.js
+++ Tutorial02/app.js
@@ -2,14 +2,17 @@
 var vertexShaderText = `
 precision mediump float;
 
-attribute vec2 vertPosition;
+attribute vec3 vertPosition;
 attribute vec3 vertColor;
 varying vec3 fragColor;
+uniform mat4 mWorld;
+uniform mat4 mView;
+uniform mat4 mProj;
 
 void main()
 {
     fragColor = vertColor;
-    gl_Position = vec4(vertPosition, 0.0, 1.0);
+    gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);
 }
 `;
 
@@ -28,6 +31,8 @@ const initDemo = function() {
 
     var canvas = document.getElementById('app-surface');
     var gl = canvas.getContext('webgl');
+    var { mat4 } = glMatrix;
+    console.log(mat4);
 
     if (!gl) {
         console.log('WebGL not supported falling back on experimental');
@@ -76,10 +81,10 @@ const initDemo = function() {
     // Create Buffer
 
     var triangleVertices = 
-    [  // X, Y,      R, G, B
-        0.0, 0.5,    1.0, 1.0, 0.0,
-        -0.5, -0.5,  0.7, 0.0, 1.0,
-        0.5, -0.5,   0.1, 1.0, 0.6
+    [  // X, Y, Z            R, G, B
+        0.0, 0.5, 0.0,     1.0, 1.0, 0.0,
+        -0.5, -0.5, 0.0,   0.7, 0.0, 1.0,
+        0.5, -0.5, 0.0,    0.1, 1.0, 0.6
     ];
 
     var triangleVertexBufferObject = gl.createBuffer();
@@ -90,10 +95,10 @@ const initDemo = function() {
     var colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
     gl.vertexAttribPointer(
         positionAttribLocation, //attribute location
-        2, //Number of elements per attribute
+        3, //Number of elements per attribute
         gl.FLOAT, // Type of elements
         gl.FALSE,
-        5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+        6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
         0 // Offset from the beginning of a single vertex to this attribute
     );
     gl.vertexAttribPointer(
@@ -101,14 +106,32 @@ const initDemo = function() {
         3, //Number of elements per attribute
         gl.FLOAT, // Type of elements
         gl.FALSE,
-        5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
-        2 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
+        6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+        3 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
     );
 
     gl.enableVertexAttribArray(positionAttribLocation);
     gl.enableVertexAttribArray(colorAttribLocation);
 
-    // Main render loop
+    // Tell OpenGL state machine which program should be active
     gl.useProgram(program);
+
+    var matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
+    var matViewUniformLocation = gl.getUniformLocation(program, 'mView');
+    var matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
+
+    var worldMatrix = new Float32Array(16);
+    var viewMatrix = new Float32Array(16);
+    var projMatrix = new Float32Array(16);    
+    mat4.identity(worldMatrix);
+    mat4.lookAt(viewMatrix, [0, 0, -5], [0, 0, 0], [0, 1, 0]);
+    mat4.perspective(projMatrix, glMatrix.glMatrix.toRadian(45), canvas.width / canvas.height, 0.1, 1000.0);
+
+    // 4fv = 4x4  Matrix Float Vector
+    gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+    gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
+    gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
+
+    // Main render loop
     gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
